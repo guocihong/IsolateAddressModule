a49bab221af96b96a688b061fde5feb411738882
guocihong <1274388603@qq.com>
2017-04-30 13:39:54 +0800

    Revert "1、第一次提交"
    This reverts commit 17949d5022e8b64cbfa5d16f964cf7bb2b8aa3e9.

--- driver/timer/timer_drv.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TIMER_DRV_H_
-#define _TIMER_DRV_H_
-
-#include "config.h"
-
-void timer0_init(void);
-
-#endif
--- driver/uart/uart_drv.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UART_DRV_H_
-#define _UART_DRV_H_
-
-#include "config.h"
-
-void uart_init(void);
-void uart2_start_trans(void);
-
-#endif
